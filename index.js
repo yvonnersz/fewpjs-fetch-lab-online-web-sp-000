401db116d2be782fdcdbd90c96b7f70ce02648ac
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 16:52:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
+
 
 function renderBooks(json) {
   const main = document.querySelector('main')
